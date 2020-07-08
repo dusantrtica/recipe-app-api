8666cd98235e25a6464e8e90c5716daa6e72637a
Dusan <dusan.trtica@hotmail.com>
2020-07-09 00:09:44 +0200

    Fixed tests for create user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -28,7 +28,7 @@ class PublicUserApiTests(TestCase):
 
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        user = get_user_model.object().get(**res.data)
+        user = get_user_model().objects.get(**res.data)
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
@@ -37,7 +37,7 @@ class PublicUserApiTests(TestCase):
         payload = {"email": 'test@test.com', 'password': 'test123'}
         create_user(**payload)
 
-        res = self.clien.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
